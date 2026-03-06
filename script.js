a7c1cbfb2373befeb297c7a840c531746d495ea8
abylaixxi <kalkaman.abyai@gmail.com>
2026-03-06 23:04:56 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+let noClicks = 0;
+
+// поздравления для каждой девушки
+const greetings = {
+  "Айзере": `Дорогая Айзере! 🌷
+Пусть этот день принесёт тебе море вдохновения и радости! 
+Твоя любовь к фото и видео делает мир ярче, а стремление учиться и развиваться в Корее обязательно принесёт успех. 
+Желаю, чтобы каждый момент был таким же красивым, как твои творческие работы! 💖`,
+
+  "Аружан": `Дорогая Аружан! 🌸
+Спасибо тебе за твою заботу, терпение и умение держать группу в порядке! 
+Пусть 8 марта подарит тебе улыбки, тепло и радость, а каждый день будет таким же вдохновляющим и светлым, как твоя энергия и лидерство! 💕`,
+
+  "Маржан": `Дорогая Маржан! 🌺
+Пусть твой дух приключений и любовь к новым впечатлениям всегда ведут к ярким моментам! 
+8 марта желаю тебе радости, смеха и красивых событий, а каждый новый день приносит столько же эмоций, сколько дарят тебе путешествия и твоя смелость открывать мир! ✨`,
+
+  "Жанел": `Дорогая Жанел! 💖
+Пусть твоя энергия, креатив и активность сияют ещё ярче! 
+8 марта желаю тебе море радости, вдохновения и лайков не только в соцсетях, но и в жизни. 
+Пусть каждый день будет таким же ярким, как твои идеи и улыбка! 🌷`
+};
+
+// открыть страницу с выбором девушки
+function openInvite(){
+  document.getElementById("notification").style.display="none";
+  document.getElementById("girls").style.display="block";
+}
+
+// выбор девушки
+function selectGirl(name, photo) {
+  document.getElementById("girls").style.display = "none";
+  document.getElementById("invite").style.display = "block";
+  document.getElementById("selectedPhoto").src = photo;
+  document.getElementById("greeting").innerText = greetings[name];
+}
+
+// кнопки
+const yes = document.getElementById("yes");
+const no = document.getElementById("no");
+
+yes.onclick = function(){
+  document.getElementById("invite").style.display="none";
+  document.getElementById("final").style.display="block";
+};
+
+no.onclick = function(){
+  noClicks++;
+  let yesSize = 18 + noClicks*10;
+  let noSize = 18 - noClicks*2;
+
+  yes.style.fontSize = yesSize+"px";
+  if(noSize > 8){
+    no.style.fontSize = noSize+"px";
+  }
+
+  moveNoButton();
+};
+
+// функция движения кнопки "НЕТ"
+function moveNoButton(){
+  const maxX = window.innerWidth - no.offsetWidth - 20;
+  const maxY = window.innerHeight - no.offsetHeight - 20;
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+
+  no.style.position = "absolute";
+  no.style.left = x + "px";
+  no.style.top = y + "px";
+}
